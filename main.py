57d64b5459b2b94ad8c3a535ce3944aaea4c7639
duyd486 <duyd486@gmail.com>
2024-10-04 22:22:01 +0700

    cv2

--- main.py
+++ main.py
@@ -1,16 +1,128 @@
 from ursina import *
+from solve_2d_cube import *
+import cv2
+from ursina.color import text_color
 import random
 
 app = Ursina(title='DUY DEP TRAI',icon='textures/my_dog_icon.ico')
 class RubikCube(Entity):
     def __init__(self, **kwargs):
         super().__init__()
-        plane = Entity(model='quad', scale=60, texture='white_cube', texture_scale=(60, 60), rotation_x=90, y=-5,
-               color=color.light_gray)
+        plane = Entity(model='quad', scale=60, texture='textures/grass.jpg', texture_scale=(60, 60), rotation_x=90, y=-5)
         sky = Entity(model='sphere', scale=150, texture='textures/test', double_sided=True)  # sky
         window.borderless = False
         EditorCamera()
         camera.world_position = (0, 0, -15)
+
+        # self.text = TextField(scale = 0.1, line_height= 1.1, character_limit=None)
+        # self.text.text = dedent("Hello")
+        # self.text.render()
+
+
+        #cv2 code
+        self.solution = []
+        self.speed_up_move = 0
+        self.normal_move = 0.5
+        self.delay_move = 0.08
+        self.step = 0
+        self.firstCall = True
+        self.values = ""
+        self.re_conv = {
+            # "F" : "F'",
+            # "F'" : "F",
+            # "F2" : "F2",
+            # "R" : "R'",
+            # "R2" : "R2",
+            # "R'" : "R",
+            # "B" : "B'",
+            # "B2" : "B2",
+            # "B'" : "B",
+            # "L" : "L'",
+            # "L2" : "L2",
+            # "L'" : "L",
+            # "U" : "U'",
+            # "U2" : "U2",
+            # "U'" : "U",
+            # "D" : "D'",
+            # "D2" : "D2",
+            # "D'" : "D",
+            "F": "f'",
+            "F'": "f",
+            "F2": "f2",
+            "R": "r'",
+            "R2": "r2",
+            "R'": "r",
+            "B": "b'",
+            "B2": "b2",
+            "B'": "b",
+            "L": "l'",
+            "L2": "l2",
+            "L'": "l",
+            "U": "u'",
+            "U2": "u2",
+            "U'": "u",
+            "D": "d'",
+            "D2": "d2",
+            "D'": "d",
+        }
+        self.state = {
+            'up': ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', ],
+            'right': ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', ],
+            'front': ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', ],
+            'down': ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', ],
+            'left': ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', ],
+            'back': ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', ]
+        }
+        self.stickers = {
+            'main': [
+                [200, 120], [300, 120], [400, 120],
+                [200, 220], [300, 220], [400, 220],
+                [200, 320], [300, 320], [400, 320]
+            ],
+            'current': [
+                [20, 20], [54, 20], [88, 20],
+                [20, 54], [54, 54], [88, 54],
+                [20, 88], [54, 88], [88, 88]
+            ],
+            'preview': [
+                [20, 130], [54, 130], [88, 130],
+                [20, 164], [54, 164], [88, 164],
+                [20, 198], [54, 198], [88, 198]
+            ],
+            'left': [
+                [50, 280], [94, 280], [138, 280],
+                [50, 324], [94, 324], [138, 324],
+                [50, 368], [94, 368], [138, 368]
+            ],
+            'front': [
+                [188, 280], [232, 280], [276, 280],
+                [188, 324], [232, 324], [276, 324],
+                [188, 368], [232, 368], [276, 368]
+            ],
+            'right': [
+                [326, 280], [370, 280], [414, 280],
+                [326, 324], [370, 324], [414, 324],
+                [326, 368], [370, 368], [414, 368]
+            ],
+            'up': [
+                [188, 128], [232, 128], [276, 128],
+                [188, 172], [232, 172], [276, 172],
+                [188, 216], [232, 216], [276, 216]
+            ],
+            'down': [
+                [188, 434], [232, 434], [276, 434],
+                [188, 478], [232, 478], [276, 478],
+                [188, 522], [232, 522], [276, 522]
+            ],
+            'back': [
+                [464, 280], [508, 280], [552, 280],
+                [464, 324], [508, 324], [552, 324],
+                [464, 368], [508, 368], [552, 368]
+            ],
+        }
+        self.check_state = []
+        self.solved = False
+
         self.load_game()
 
 
@@ -21,7 +133,7 @@ class RubikCube(Entity):
         self.rotation_axes = {'LEFT': 'x', 'RIGHT': 'x', 'UP': 'y', 'DOWN': 'y', 'FRONT': 'z', 'BACK': 'z'}
         self.cubes_side_positons = {'LEFT': self.LEFT, 'DOWN': self.DOWN, 'RIGHT': self.RIGHT, 'FRONT': self.FRONT,
                                     'BACK': self.BACK, 'UP': self.UP}
-        self.animation_time = .2
+        self.animation_time = 0.2
         self.action_trigger = True
         print("load")
 
@@ -29,7 +141,7 @@ class RubikCube(Entity):
     def toggle_animation_trigger(self):
         self.action_trigger = not self.action_trigger
 
-    def rotate_side(self, side_name):
+    def rotate_side(self, side_name, degree):
         self.action_trigger = False
         cube_positions = self.cubes_side_positons[side_name]
         rotation_axis = self.rotation_axes[side_name]
@@ -37,8 +149,8 @@ class RubikCube(Entity):
         for cube in self.CUBES:
             if cube.position in cube_positions:
                 cube.parent = self.PARENT
-                eval(f'self.PARENT.animate_rotation_{rotation_axis}(90, duration=self.animation_time)')
-        invoke(self.toggle_animation_trigger, delay=self.animation_time + 0.11)
+                eval(f'self.PARENT.animate_rotation_{rotation_axis}(degree, duration=self.animation_time)')
+        invoke(self.toggle_animation_trigger, delay=self.animation_time + self.delay_move)
 
 
     def reparent_to_scene(self):
@@ -59,21 +171,159 @@ class RubikCube(Entity):
         self.UP = {Vec3(x, 1, z) for x in range(-1, 2) for z in range(-1, 2)}
         self.SIDE_POSITIONS = self.LEFT | self.DOWN | self.FRONT | self.BACK | self.RIGHT | self.UP
 
+    def rubik_detect(self):
+        print("Hi im rubik detect")
+        print("Wait a second")
+        cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)
+        while True:
+            hsv=[]
+            current_state=[]
+            ret,img=cap.read()
+            #img=cv2.flip(img,1)
+            frame = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            mask = np.zeros(frame.shape, dtype=np.uint8)
 
+            draw_stickers(img,self.stickers,'main')
+            draw_stickers(img,self.stickers,'current')
+            draw_preview_stickers(preview,self.stickers)
+            fill_stickers(preview,self.stickers,self.state)
+            texton_preview_stickers(preview,self.stickers)
+            for i in range(9):
+                hsv.append(frame[self.stickers['main'][i][1]+10][self.stickers['main'][i][0]+10])
+
+            a=0
+            for x,y in self.stickers['current']:
+                color_name=color_detect(hsv[a][0],hsv[a][1],hsv[a][2])
+                cv2.rectangle(img,(x,y),(x+30,y+30),color[color_name],-1)
+                a+=1
+                current_state.append(color_name)
+
+            k = cv2.waitKey(5) & 0xFF
+            if k == ord('q'):
+                break
+            elif k ==ord('u'):
+                self.state['up']=current_state
+                self.check_state.append('u')
+            elif k ==ord('r'):
+                self.check_state.append('r')
+                self.state['right']=current_state
+            elif k ==ord('l'):
+                self.check_state.append('l')
+                self.state['left']=current_state
+            elif k ==ord('d'):
+                self.check_state.append('d')
+                self.state['down']=current_state
+            elif k ==ord('f'):
+                self.check_state.append('f')
+                self.state['front']=current_state
+            elif k ==ord('b'):
+                self.check_state.append('b')
+                self.state['back']=current_state
+            elif k == ord('\r'):
+                # process(["R","R'"])
+                if len(set(self.check_state))==6:
+                    #Scan xong, lưu kết quả vào biến state
+                    print("hi")
+                    #lay cong thuc giai, dao nguoc va cho khoi rubik chay
+                    try:
+                        values = detect_solve(self.state)
+                        self.solution = values
+                        #self.solution = list(values.split(" "))
+                        values = list(values.split(" "))
+                        values.reverse()
+                        print(values)
+                        self.delay_move = 0
+                        for value in values:
+                            value = self.re_conv[value]
+                            lis_value = list(value)
+                            if lis_value[-1] == '2':
+                                lis_value.pop(-1)
+                                value = ''.join(lis_value)
+                                self.move(value)
+                                self.move(value)
+                                print('moving ' + value)
+                            else:
+                                self.move(value)
+                                print('moving ' + value)
+                        self.delay_move = 0.08
+                        break
+
+                    except:
+                        print("rubik error, scan again, maybe wrong something idiot!")
+
+
+                else:
+                    #Chưa scan xong, thiếu mặt cần scan
+                    print("")
+                    print("left to scan:",6-len(set(self.check_state)))
+            cv2.imshow('preview',preview)
+            cv2.imshow('frame',img[0:500,0:500])
+        cv2.destroyAllWindows()
+
+
+    def move(self, value):
+        if value == 'l' :
+            self.animation_time = self.speed_up_move
+            self.rotate_side('LEFT',-90)
+        if value == 'd':
+            self.animation_time = self.speed_up_move
+            self.rotate_side('DOWN',-90)
+        if value == 'r':
+            self.animation_time = self.speed_up_move
+            self.rotate_side('RIGHT',90)
+        if value == 'f':
+            self.animation_time = self.speed_up_move
+            self.rotate_side('FRONT',90)
+        if value == 'b':
+            self.animation_time = self.speed_up_move
+            self.rotate_side('BACK',-90)
+        if value == 'u':
+            self.animation_time = self.speed_up_move
+            self.rotate_side('UP',90)
+
+        if value == "l'" :
+            self.animation_time = self.speed_up_move
+            self.rotate_side('LEFT',90)
+        if value == "d'":
+            self.animation_time = self.speed_up_move
+            self.rotate_side('DOWN',90)
+        if value == "r'":
+            self.animation_time = self.speed_up_move
+            self.rotate_side('RIGHT',-90)
+        if value == "f'":
+            self.animation_time = self.speed_up_move
+            self.rotate_side('FRONT',-90)
+        if value == "b'":
+            self.animation_time = self.speed_up_move
+            self.rotate_side('BACK',90)
+        if value == "u'":
+            self.animation_time = self.speed_up_move
+            self.rotate_side('UP',-90)
 
     def input(self, key):
+        if key == 'o':
+            self.rubik_detect()
+        if key == 's':
+            print(self.solution)
         if key == 'l' and self.action_trigger:
-            self.rotate_side('LEFT')
+            self.animation_time = self.normal_move
+            self.rotate_side('LEFT',-90)
         if key == 'd' and self.action_trigger:
-            self.rotate_side('DOWN')
+            self.animation_time = self.normal_move
+            self.rotate_side('DOWN',-90)
         if key == 'r' and self.action_trigger:
-            self.rotate_side('RIGHT')
+            self.animation_time = self.normal_move
+            self.rotate_side('RIGHT',90)
         if key == 'f' and self.action_trigger:
-            self.rotate_side('FRONT')
+            self.animation_time = self.normal_move
+            self.rotate_side('FRONT',90)
         if key == 'b' and self.action_trigger:
-            self.rotate_side('BACK')
+            self.animation_time = self.normal_move
+            self.rotate_side('BACK',-90)
         if key == 'u' and self.action_trigger:
-            self.rotate_side('UP')
+            self.animation_time = self.normal_move
+            self.rotate_side('UP',90)
 
 
 
---
+++ solve_2d_cube.py
@@ -0,0 +1,97 @@
+import cv2
+import numpy as np
+import kociemba as Cube
+
+
+
+sign_conv = {
+    'green': 'F',
+    'white': 'U',
+    'blue': 'B',
+    'red': 'R',
+    'orange': 'L',
+    'yellow': 'D'
+}
+
+color = {
+    'red': (0, 0, 255),
+    'orange': (0, 165, 255),
+    'blue': (255, 0, 0),
+    'green': (0, 128, 0),
+    'white': (255, 255, 255),
+    'yellow': (0, 255, 255),
+    'black': (0, 0, 0),
+}
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+textPoints = {
+    'up': [['U', 242, 202], ['W', (255, 255, 255), 260, 208]],
+    'right': [['R', 380, 354], ['R', (0, 0, 255), 398, 360]],
+    'front': [['F', 242, 354], ['G', (0, 255, 0), 260, 360]],
+    'down': [['D', 242, 508], ['Y', (0, 255, 255), 260, 514]],
+    'left': [['L', 104, 354], ['O', (0, 165, 255), 122, 360]],
+    'back': [['B', 518, 354], ['B', (255, 0, 0), 536, 360]],
+}
+
+
+
+def detect_solve(state):
+    raw = ''
+    for i in state:
+        for j in state[i]:
+            raw += sign_conv[j]
+    #print("answer:", Cube.solve(raw))
+    return Cube.solve(raw)
+
+
+def color_detect(h, s, v):
+    # print(h,s,v)
+    if (h > 140 and h <= 180 and s > 40) or (h > 0 and h < 5 and s > 40):
+        return 'red'
+    elif h >= 5 and h < 21 and s > 40:
+        return 'orange'
+    elif h > 20 and h < 40 and s > 40:
+        return 'yellow'
+    elif h > 40 and h < 90 and s > 40:
+        return 'green'
+    elif h > 90 and h < 140 and s > 40:
+        return 'blue'
+    elif s <= 40:
+        return 'white'
+
+    return 'black'
+
+
+def draw_stickers(frame, stickers, name):
+    for x, y in stickers[name]:
+        cv2.rectangle(frame, (x, y), (x + 30, y + 30), (255, 255, 255), 2)
+
+
+def draw_preview_stickers(frame, stickers):
+    stick = ['front', 'back', 'left', 'right', 'up', 'down']
+    for name in stick:
+        for x, y in stickers[name]:
+            cv2.rectangle(frame, (x, y), (x + 40, y + 40), (255, 255, 255), 2)
+
+
+def texton_preview_stickers(frame, stickers):
+    stick = ['front', 'back', 'left', 'right', 'up', 'down']
+    for name in stick:
+        for x, y in stickers[name]:
+            sym, x1, y1 = textPoints[name][0][0], textPoints[name][0][1], textPoints[name][0][2]
+            cv2.putText(preview, sym, (x1, y1), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
+            sym, col, x1, y1 = textPoints[name][1][0], textPoints[name][1][1], textPoints[name][1][2], \
+            textPoints[name][1][3]
+            cv2.putText(preview, sym, (x1, y1), font, 0.5, col, 1, cv2.LINE_AA)
+
+
+def fill_stickers(frame, stickers, sides):
+    for side, colors in sides.items():
+        num = 0
+        for x, y in stickers[side]:
+            cv2.rectangle(frame, (x, y), (x + 40, y + 40), color[colors[num]], -1)
+            num += 1
+
+
+
+preview = np.zeros((700,800,3), np.uint8)
